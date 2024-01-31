b73cac40bc9550b4a8ff52d7c80972c61e6cfdba
mitex22 <mitex22@abv.bg>
2024-01-31 16:27:10 +0200

    show movies dynamically on home

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -8,7 +8,7 @@ router.get('/create', (req, res) => {
 router.post('/create', (req, res) => {
     const newMovie = req.body;
     movieService.create(newMovie);
-    
+    res.redirect('/');
 });
 
 module.exports = router;
