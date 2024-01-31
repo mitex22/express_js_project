28e0d2a6f0f6cd423f22e3159180c3555bc754e5
mitex22 <mitex22@abv.bg>
2024-01-31 13:48:41 +0200

    add main layout

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('home', { layout: false });
+    res.render('home');
 });
 
 router.get('/about', (req, res) => {
-    res.render('about', { layout: false });
+    res.render('about');
 });
 
 module.exports = router;
