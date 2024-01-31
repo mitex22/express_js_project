351a4a717337f4a14172962066efc3141385990c
mitex22 <mitex22@abv.bg>
2024-01-31 15:59:35 +0200

    create movie service

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+const movieService = require('../services/movieService');
 
 router.get('/create', (req, res) => {
     res.render('create.hbs');
 });
 
 router.post('/create', (req, res) => {
+    const newMovie = req.body;
+    movieService.create(newMovie);
     
 });
 
