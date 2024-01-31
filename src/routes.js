ab4eff2cee1a55541f2285f7e306f519c4b7e856
mitex22 <mitex22@abv.bg>
2024-01-31 14:24:22 +0200

    add 404

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 
-router.use(homeController);
 router.use(movieController);
+// homeController always at the bottom due to 404 page check 
+router.use(homeController);
 
 module.exports = router;
