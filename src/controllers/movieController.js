a303b47c7100459e36e2e028f3efa4e49e69a649
mitex22 <mitex22@abv.bg>
2024-02-10 00:00:35 +0200

    add edit view

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -50,4 +50,10 @@ router.post('/movies/:movieId/attach', async (req, res) => {
     res.redirect(`/movies/${movieId}/attach`);
 });
 
+router.get('/movies/:movieId/edit', (req, res) => {
+    const movieId = req.params.movieId;
+
+    res.render('movie/edit');
+})
+
 module.exports = router;
