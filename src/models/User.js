0a71eceaf89a6eadac2c1282afa6bca750faa71d
mitex22 <mitex22@abv.bg>
2024-02-09 21:54:27 +0200

    implement password hashing before saving into DB

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,6 +12,28 @@ const userSchema = new mongoose.Schema({
         required: true
     },
 });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 12);
+
+    this.password = hash;
+});
+
+// AI GENERATED
+// userSchema.methods.checkPassword = async function(password){
+//     return await bcrypt.compare(password, this.password);
+// };
+
+// // hash the password before saving it to the database
+// userSchema.pre('save', async function () {
+//     if (!this.isModified('password')) return next();
+//     try{
+//         const salt = await bcrypt.genSalt(10);
+//         this.password = await bcrypt.hash(this.password, salt);
+//     }catch(err){
+//         console.log(err);
+//     }
+// });
 
 const User = mongoose.model('User', userSchema);
 
