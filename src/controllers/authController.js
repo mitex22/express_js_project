21361266f0e11f7133693cda4cfb7e9c3d987aee
mitex22 <mitex22@abv.bg>
2024-02-09 23:41:29 +0200

    return auth token in a cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,8 +22,8 @@ router.post('/login', async (req, res) => {
 
     const token = await authService.login(email, password);
 
-    console.log(token);
-
+    res.cookie('auth', token);
+    
     res.redirect('/');
 });
 
