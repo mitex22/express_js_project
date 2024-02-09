23032fa3eda9c7623ca6ae29109daaeaf5971e05
mitex22 <mitex22@abv.bg>
2024-02-09 23:34:38 +0200

    add cookie-parser lib

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,9 +1,11 @@
 const experss = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 function configExpress(app) {
     app.use(experss.static(path.resolve('src/public')));
     app.use(experss.urlencoded({ extended: false }))
+    app.use(cookieParser())
 
     return app;
 }
